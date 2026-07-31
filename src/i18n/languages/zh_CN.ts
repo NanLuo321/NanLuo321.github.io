b43dca3eefd1e0d3159b5626950ab657b92924b5
NanLuo321 <updonw2@google.com>
2026-07-31 14:09:15 +0800

    更改了手机UI的位置，修复了所有已知问题

--- src/i18n/languages/zh_CN.ts
+++ src/i18n/languages/zh_CN.ts
@@ -345,7 +345,7 @@ export const zh_CN: Translation = {
 
 	// 站点信息
 	[Key.siteInfo]: "当前时间",
-	[Key.siteInfoBuildTime]: "构建时间",
+	[Key.siteInfoBuildTime]: "当前时间",
 	[Key.siteInfoBuildPlatform]: "构建平台",
 	[Key.siteInfoBlogVersion]: "博客版本",
 	[Key.siteInfoAstroVersion]: "Astro",
